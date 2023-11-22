a4c277800fe8b69ef0756a4476af1d6f4e6d0c0f
Wama Nigam <wamanigam99@gmail.com>
2023-11-22 21:48:48 +0530

    first api call

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
+
+const navbarStyle = {
+    backgroundColor: '#B12F3F',
+    color: 'white',
+};
 
 const Header = (props) => {
     return (
-        <Navbar bg="light" data-bs-theme="light">
-          <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+       
+        <Navbar style={navbarStyle} data-bs-theme="light">
+             <Container>
+          <Navbar.Brand href="/" style={navbarStyle}>{props.title}</Navbar.Brand>
+          </Container>
         </Navbar>
+        
     )
 };
 
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+
+const Search = ({word, setWord, handleSubmit}) => {
+  return (
+    <Container className="mt-4 ">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6}>
+          <Form onSubmit={handleSubmit}>
+            <Row>
+              <Col xs={9}>
+                <Form.Control 
+                    type = "text"
+                    value={word}
+                    onChange={(e) => setWord(e.target.value)}
+                    placeholder="Search for new Image" 
+                />
+              </Col>
+              <Col>
+                <Button variant="dark" type="submit">Search</Button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Search;
